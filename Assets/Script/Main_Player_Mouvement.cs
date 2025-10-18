584c97ca5156f7dc59d046e1258ddd7821a87eb7
UnMaxdeUwU <quentinbres3@gmail.com>
2025-10-18 18:17:18 +0200

    ImportSprite, Spike, GameOver

--- Assets/Script/Main_Player_Mouvement.cs
+++ Assets/Script/Main_Player_Mouvement.cs
@@ -3,20 +3,23 @@ using UnityEngine;
 public class Main_Player_Mouvement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float Strength_ImpulseSpeed = 1f;
+    private float Strength_ImpulseSpeed = 3f;
     private float Strenght_ImpulseCurrent = 0f;
-    private float Strength_ImpuleMax = 5f;
+    private float Strength_ImpuleMax = 20f;
+    private Vector2 Start_Position;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Start_Position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {   
         Vector2 Main_Position = transform.position;
+        
         Vector2 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = MousePosition - Main_Position;
         if (Input.GetMouseButton(0))
@@ -39,5 +42,17 @@ public class Main_Player_Mouvement : MonoBehaviour
     {
         //rb.AddForce(transform.right * current);
         rb.AddForce(direction * current, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<Spike>() != null)
+        {
+            ResetPosition();
+        }
+    }
+    private void ResetPosition()
+    {
+        rb.position = Start_Position;
     }
 }
