93abce00da9e99d14659de7595c547ed679d2b53
UnMaxdeUwU <quentinbres3@gmail.com>
2025-10-25 20:15:33 +0200

    Update Tourelle, turn, shoot

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Turret : MonoBehaviour
 {   
@@ -11,6 +14,8 @@ public class Turret : MonoBehaviour
     [SerializeField] private Transform TurretSprite;
     [SerializeField] private Transform _SpawnPoint;
     [SerializeField] private Projectile _projectilePrefab;
+    private Coroutine coroutineBullet;
+    private Vector2 myTransform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,22 +33,31 @@ public class Turret : MonoBehaviour
             _positionTurret = transform.position;
             Vector2 target = _positionPlayer.position;  
             Vector2 direction = target - _positionTurret;
+            myTransform = direction;
+            
             float angle = Mathf.Atan2(direction.y , direction.x) * Mathf.Rad2Deg;
             //transform.rotation = Quaternion.Euler(0, 0, angle + RotationCorrection);
             TurretSprite.transform.rotation = Quaternion.Euler(0, 0, angle + RotationCorrection);
-            Debug.Log(angle);
-            Instantiate(_projectilePrefab, _SpawnPoint.position, Quaternion.identity);
+            //Debug.Log(angle);
+            
+            
         }
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Main_Player_Mouvement>() != null)
-        {
+        {   
             playerEnter = true;
             _positionPlayer = other.transform;
-        }
+            if (coroutineBullet == null)
+            {
+                coroutineBullet = StartCoroutine(BulletTimer());
+            }
 
+        }
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -52,11 +66,37 @@ public class Turret : MonoBehaviour
         {   
             playerEnter = true;
             _positionPlayer = other.transform;
+
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        playerEnter = false;
+        if (other.GetComponent<Main_Player_Mouvement>() == null)
+        {
+            playerEnter = false;
+            if (coroutineBullet != null)
+            {   
+                Debug.Log("Stop");
+                StopCoroutine(BulletTimer());
+                coroutineBullet = null;
+            }
+        }
+
+
+    }
+
+    IEnumerator BulletTimer()
+    {
+
+        while (playerEnter)
+        {   
+            Instantiate(_projectilePrefab, _SpawnPoint.position, Quaternion.identity).Init(myTransform);
+            Debug.Log("CoroutineStart");
+            yield return new WaitForSeconds(3);
+        }
+
+        coroutineBullet = null;
+
     }
 }
